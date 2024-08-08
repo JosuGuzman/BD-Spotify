6d77b47b3f2713dd9d771024cc0361dbfb872c93
rene <rene.terrazaset12d1@gmail.com>
2024-08-08 16:03:03 -0300

    ultimos insert y call

--- scripts sql/Insert.sql
+++ scripts sql/Insert.sql
@@ -1,11 +1,10 @@
 USE Spotify;
 
 -- Tabla Artista
-CALL altaArtista ();
-CALL altaArtista ();
-CALL altaArtista ();
-CALL altaArtista ();
-
+ CALL altaArtista('TripleT', 'Miguel', 'Verduguez', @idArtista1);
+ CALL altaArtista('El Chapo', 'Josu', 'Duran', @idArtista2);
+ CALL altaArtista('La Maquinaria', 'Rene', 'Terrazas', @idArtista3);
+ CALL altaArtista('El Renacido', 'Leonardo', 'Cheng', @idArtista4);
 -- Tabla Albun
  CALL altaAlbun (@idAlbum1, 'Luz y Sombra', @idArtista1);
  CALL altaAlbun (@idAlbum2, 'Caminos Cruzados', @idArtista2);
@@ -13,10 +12,10 @@ CALL altaArtista ();
  CALL altaAlbun (@idAlbum4, 'Ecos del Pasado', @idArtista4);
 
 -- Tabla Nacionalidad
-CALL altaNacionalidad ('Argentina', @idNacionalidad1);
-CALL altaNacionalidad ('Brasil', @idNacionalidad2 );
-CALL altaNacionalidad ('U.R.S.S.', @idNacionalidad3);
-CALL altaNacionalidad ('Bolivia', @idNacionalidad4);
+CALL altaNacionalidad ('Argentina', @idNacionalidadArgentina);
+CALL altaNacionalidad ('Brasil', @idNacionalidadBrasil );
+CALL altaNacionalidad ('U.R.S.S.', @idNacionalidadURSS);
+CALL altaNacionalidad ('Bolivia', @idNacionalidadBolivia);
 
 -- Tabla Usuario
 CALL altaUsuario ("Miguel", "miguelito@gmail.com", "Deadpool3saliomal", 1, @idUsuarioMiguel);
@@ -25,28 +24,29 @@ CALL altaUsuario ("Rene","rene@gmail.com","Totoro",3,@idUsuarioRene);
 CALL altaUsuario ("Cheng","chengleonardo@gmail.com","capitalismonofunciona",4,@idUsuarioCheng);
 
 -- Tabla Genero
-CALL altaGenero ();
-CALL altaGenero ();
-CALL altaGenero ();
-CALL altaGenero ();
-
+CALL altaGenero('Hip-hop/Rap', @idGenero1);
+CALL altaGenero('Jazz', @idGenero2);
+CALL altaGenero('Reggae', @idGenero3);
+CALL altaGenero('Ranchera', @idGenero4);
 -- Tabla Cancion
-CALL altaCancion ();
-CALL altaCancion ();
-CALL altaCancion ();
-CALL altaCancion ();
+CALL altaCancion(@idCancion1, 'It\'s Over, Isn\'t It', '00:02:17', @idAlbum3, @idArtista1, @idGenero1);
+CALL altaCancion(@idCancion2, 'René', '00:07:41', @idAlbum2, @idArtista2, @idGenero2);
+CALL altaCancion(@idCancion3, 'Como Estrella', '00:03:40', @idAlbum1, @idArtista3, @idGenero3);
+CALL altaCancion(@idCancion4, 'Estos Celos', '00:03:10', @idAlbum4, @idArtista4, @idGenero4);
+
 
 -- Tabla Historial Reproduccion
-CALL altaHistorial_reproduccion ();
-CALL altaHistorial_reproduccion ();
-CALL altaHistorial_reproduccion ();
-CALL altaHistorial_reproduccion ();
+CALL altaHistorial_reproduccion(@idHistorial1, @idUsuario1, @idCancion1, '2024-07-01 10:00:00');
+CALL altaHistorial_reproduccion(@idHistorial2, @idUsuario2, @idCancion2, '2024-07-01 11:00:00');
+CALL altaHistorial_reproduccion(@idHistorial3, @idUsuario3, @idCancion3, '2024-07-01 12:00:00');
+CALL altaHistorial_reproduccion(@idHistorial4, @idUsuario4, @idCancion4, '2024-07-01 13:00:00');
 
 -- Tabla Playlist
-CALL altaPlaylist ();
-CALL altaPlaylist ();
-CALL altaPlaylist ();
-CALL altaPlaylist ();
+CALL altaPlaylist('Éxitos de Rock', @idUsuario1, @idPlaylist1);
+CALL altaPlaylist('Clásicos del Pop', @idUsuario2, @idPlaylist2);
+CALL altaPlaylist('Vibras de Jazz', @idUsuario3, @idPlaylist3);
+CALL altaPlaylist('Ritmos Chill', @idUsuario4, @idPlaylist4);
+
 
 -- Tabla Playlist_Cancion
 INSERT INTO Playlist_Canciones(idPlaylist,idCancion,FechaIngresion)
@@ -58,10 +58,12 @@ INSERT INTO Playlist_Canciones(idPlaylist,idCancion,FechaIngresion)
 
 
 -- Tabla Suscripcion
-CALL altaSuscripcion ();
-CALL altaSuscripcion ();
-CALL altaSuscripcion ();
-CALL altaSuscripcion ();
+CALL altaSuscripcion (@idSuscripcionMensual,1,8,’Mensual’);
+CALL altaSuscripcion (@idSuscripcionTrimestral,3,15,’Trimestral’);
+CALL altaSuscripcion (@idSuscripcionCuatrimestrall,4,20,’Cuatrimestral’);
+CALL altaSuscripcion (@idSuscripcionBimestral,2,12,’Bimestral);
+
+
 
 -- Tabla Suscripcion_Usuario
 INSERT INTO Usuario_Suscripcion (idUsuario,idSuscripcion,tipoSuscripcion,FechaInicio)
