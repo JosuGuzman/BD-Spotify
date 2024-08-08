15efdba851bc3f7b83ebada27021cbf41f6dccf5
lolita0502 <josu.guzmanet12d1@gmail.com>
2024-08-08 14:41:42 -0300

    CALL de Usuario|

--- scripts sql/Insert.sql
+++ scripts sql/Insert.sql
@@ -7,6 +7,10 @@ USE Spotify;
 -- Tabla Nacionalidad
 
 -- Tabla Usuario
+CALL altaUsuario ("Miguel", "miguelito@gmail.com", "Deadpool3saliomal", 1, @idUsuarioMiguel);
+CALL altaUsuario ("Josu","josu@gmail.com","OMORIoyasumi",2,@idUsuarioJosu);
+CALL altaUsuario ("Rene","rene@gmail.com","Totoro",3,@idUsuarioRene);
+CALL altaUsuario ("Cheng","chengleonardo@gmail.com","capitalismonofunciona",4,@idUsuarioCheng);
 
 -- Tabla Genero
 
