932f77a0e8be5a1bb88820b7cfad19ddd941879b
lolita0502 <josu.guzmanet12d1@gmail.com>
2024-08-08 14:58:21 -0300

    los demas CALL

--- scripts sql/Insert.sql
+++ scripts sql/Insert.sql
@@ -25,15 +25,35 @@ CALL altaUsuario ("Rene","rene@gmail.com","Totoro",3,@idUsuarioRene);
 CALL altaUsuario ("Cheng","chengleonardo@gmail.com","capitalismonofunciona",4,@idUsuarioCheng);
 
 -- Tabla Genero
+CALL altaGenero ();
+CALL altaGenero ();
+CALL altaGenero ();
+CALL altaGenero ();
 
 -- Tabla Cancion
+CALL altaCancion ();
+CALL altaCancion ();
+CALL altaCancion ();
+CALL altaCancion ();
 
 -- Tabla Historial Reproduccion
+CALL altaHistorial_reproduccion ();
+CALL altaHistorial_reproduccion ();
+CALL altaHistorial_reproduccion ();
+CALL altaHistorial_reproduccion ();
 
 -- Tabla Playlist
+CALL altaPlaylist ();
+CALL altaPlaylist ();
+CALL altaPlaylist ();
+CALL altaPlaylist ();
 
 -- Tabla Playlist_Cancion
 
 -- Tabla Suscripcion
+CALL altaSuscripcion ();
+CALL altaSuscripcion ();
+CALL altaSuscripcion ();
+CALL altaSuscripcion ();
 
 -- Tabla Suscripcion_Usuario
