08cfc7ee5e12abc1628eb211d28b358a29a1cce7
lolita0502 <josu.guzmanet12d1@gmail.com>
2024-08-08 17:13:15 -0300

    Update PlayList.cs

--- src/Spotify.Core/PlayList.cs
+++ src/Spotify.Core/PlayList.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace Spotify.Core;
 
-namespace Spotify.Core
+public class PlayList
 {
-    public class PlayList
-    {
-        public int IdPlaylist {get;set;}
-        public required string Nombre {get;set;}
-        public int IdUsuario {get;set;}
-    }
+    public int IdPlaylist {get;set;}
+    public required string Nombre {get;set;}
+    public int IdUsuario {get;set;}
 }
