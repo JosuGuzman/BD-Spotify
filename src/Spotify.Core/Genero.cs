3d0253c0d34a9ebbe93fa82307339af694c393c9
lolita0502 <josu.guzmanet12d1@gmail.com>
2024-08-08 15:42:19 -0300

    clase Genero

--- src/Spotify.Core/Genero.cs
+++ src/Spotify.Core/Genero.cs
@@ -4,4 +4,10 @@ public class Genero
 {
     public sbyte IdGenero {get; set;}
     public required string Generos {get; set;}
+    
+    public Genero (sbyte idGenero, string generos)
+    {
+        IdGenero = idGenero;
+        Generos = generos;
+    }
 }
