e1852d2d36e59fdf8287767cb21fac0e04a68fe8
lolita0502 <josu.guzmanet12d1@gmail.com>
2024-08-12 14:38:45 -0300

    Delete HistorialReproduccion.cs

--- src/Spotify.Core/HistorialReproduccion.cs
+++
@@ -1,9 +0,0 @@
-namespace Spotify.Core;
-
-public class HistorialReproduccion
-{
-    public int idHistorial {get;set;}
-    public required Usuario usuario {get;set;}
-    public required Cancion cancion {get;set;}
-    public DateTime FechaReproduccion {get;set;}
-}
