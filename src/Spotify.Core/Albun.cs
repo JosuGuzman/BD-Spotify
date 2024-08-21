efe72b58934c44a8a809d14ccada58121c3b2249
Fenrir <josu.guzmanet12d1@gmail.com>
2024-08-21 16:38:27 -0300

    arregladito

--- src/Spotify.Core/Albun.cs
+++ src/Spotify.Core/Albun.cs
@@ -5,6 +5,6 @@ namespace Spotify.Core
         public int IdAlbum {get;set;}
         public required string Titulo {get;set;}
         public DateTime FechaLanzamiento {get;set;}
-        public required Artista artista {get;set;}
+        public required Artista Artistas {get;set;}
     }
 }
