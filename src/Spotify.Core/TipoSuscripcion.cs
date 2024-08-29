18fa6b35aaba121f47219f0323f70d4b0e6a4802
rene <rene.terrazaset12d1@gmail.com>
2024-08-29 14:18:08 -0300

    comit clases

--- src/Spotify.Core/TipoSuscripcion.cs
+++ src/Spotify.Core/TipoSuscripcion.cs
@@ -3,7 +3,7 @@ namespace Spotify.Core;
 public class TipoSuscripcion
 {
     public int IdSuscripcion {get;set;}
-    public sbyte Duracion {get;set;}
-    public sbyte Costo {get;set;}
+    public uint Duracion {get;set;}
+    public uint Costo {get;set;}
     public required string Tipo {get;set;}
 }
