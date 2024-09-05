ecdba48d2957916b8a5325c6f6a698cde64bffb4
JosuGuzman <josu.guzmanet12d1@gmail.com>
2024-09-05 14:16:19 -0300

    agregando pequeños detalles

--- src/Spotify.ReposDapper.Test/RepoGeneroTest.cs
+++ src/Spotify.ReposDapper.Test/RepoGeneroTest.cs
@@ -1,11 +1,11 @@
 using Spotify.Core.Persistencia;
 
 namespace Spotify.ReposDapper.Test;
-
 public class RepoGeneroTest : TestBase
 {
     IRepoGenero _repo;
-    public RepoGeneroTest() : base() => _repo = new RepoGenero(Conexion);
+    public RepoGeneroTest() : base()
+        => _repo = new RepoGenero(Conexion);
 
     [Fact]
     public void ListarOK()
