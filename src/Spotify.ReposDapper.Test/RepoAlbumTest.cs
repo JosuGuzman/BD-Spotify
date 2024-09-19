aa9d5c0221bd21eec16f683f8df1f00fe58738cb
JosuGuzman <josu.guzmanet12d1@gmail.com>
2024-09-19 14:30:36 -0300

    Update RepoAlbumTest.cs

--- src/Spotify.ReposDapper.Test/RepoAlbumTest.cs
+++ src/Spotify.ReposDapper.Test/RepoAlbumTest.cs
@@ -1,4 +1,5 @@
 using Spotify.Core;
+using Spotify.Core.Persistencia;
 namespace Spotify.ReposDapper.Test;
 
 public class RepoAlbumTest : TestBase
@@ -16,19 +17,38 @@ public class RepoAlbumTest : TestBase
     [Fact]
     public void ListarOK()
     {
-        var album = _repoAlbum.Obtener();
-        Assert.Contains(album, a => a.Titulo == "Ecos del Pasado");
+        var albunes = _repoAlbum.Obtener();
+        Assert.Contains(albunes, a => a.Titulo == "Ecos del Pasado");
     }
  
-        [Fact]
+    [Fact]
     public void AltaAlbum()
     {   
         var artis = _repoArtista.Obtener().First(); // Obtiene un artista para la prueba
         var nuevoAlbum = new Album { Titulo = "Baladas del Mar", FechaLanzamiento = DateTime.Now, artista = artis };
         var IdAlbum = _repoAlbum.Alta(nuevoAlbum);
     
-        var Albunes = _repoAlbum.Obtener();
-        Assert.Contains(Albunes, a => a.Titulo == "Baladas del Mar");
+        var albunes = _repoAlbum.Obtener();
+        Assert.Contains(albunes, a => a.Titulo == "Baladas del Mar");
     }
 
-}
+    [Fact]
+    public void EliminarAlbum()
+    {
+
+        // Obtener un álbum existente de la base de datos
+        var albumesAntes = _repoAlbum.Obtener();
+        var albumAEliminar = albumesAntes.First(); // Suponiendo que hay al menos un álbum
+
+        // Asegurandonos de que el álbum que vamos a eliminar existe
+        Assert.NotNull(albumAEliminar);
+
+        // Eliminar el álbum
+        _repoAlbum.Eliminar(albumAEliminar.idAlbum);
+
+        // Verificar que el álbum ya no está presente
+        var albumesDespues = _repoAlbum.Obtener();
+        Assert.DoesNotContain(albumesDespues, a => a.idAlbum == albumAEliminar.idAlbum);
+    }   
+
+}   
