068c5dcc521762cd3beb98a8a0418053218b361c
JosuGuzman <josu.guzmanet12d1@gmail.com>
2024-09-23 13:44:59 -0300

    Update PlayList.cs

--- src/Spotify.Core/PlayList.cs
+++ src/Spotify.Core/PlayList.cs
@@ -5,5 +5,5 @@ public class PlayList
     public uint idPlaylist {get;set;}
     public required string Nombre {get;set;}
     public required Usuario usuario {get;set;}
-    public  List<Cancion> Canciones {get;set;}
+    public required List<Cancion> Canciones {get;set;}
 }
