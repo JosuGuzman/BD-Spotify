061762c6aad1e60bc3418cfbf1990b441e298bb0
JosuGuzman <josu.guzmanet12d1@gmail.com>
2024-10-14 16:42:22 -0300

    Update DCL.sql

--- scripts sql/DCL.sql
+++ scripts sql/DCL.sql
@@ -1,5 +1,28 @@
-CREATE USER 'nombre_usuario'@'localhost' IDENTIFIED BY 'contraseña';
+-- Creación de usuarios
+CREATE USER 'Admin'@'localhost' IDENTIFIED BY 'adiMn456?';
+CREATE USER 'Admin'@'%' IDENTIFIED BY 'Adimer213#';
+CREATE USER ''@'' IDENTIFIED BY 'artista456';
+CREATE USER 'CreadorPlayList'@'localhost' IDENTIFIED BY 'playlist789';
+CREATE USER 'reporte_viewer'@'localhost' IDENTIFIED BY 'reporte321';
 
-GRANT SELECT, INSERT, UPDATE ON 5to_Spotify.* TO 'nombre_usuario'@'localhost';
+-- Asignación de permisos para Admin
+GRANT ALL PRIVILEGES ON 5to_Spotify.* TO 'Admin'@'localhost';
+GRANT ALL PRIVILEGES ON 5to_Spotify.* TO 'Admin'@'%';
 
-DROP USER 'nombre_usuario'@'localhost';
+-- Asignación de permisos para Manager
+GRANT SELECT, INSERT, UPDATE ON 5to_Spotify.Artista TO 'Manager'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON 5to_Spotify.Album TO 'Manager'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON 5to_Spotify.Cancion TO 'Manager'@'localhost';
+
+-- Asignación de permisos para CreadorPlayList
+GRANT SELECT ON 5to_Spotify.Cancion TO 'CreadorPlayList'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON 5to_Spotify.Playlist TO 'CreadorPlayList'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON 5to_Spotify.Playlist_Cancion TO 'CreadorPlayList'@'localhost';
+
+-- Asignación de permisos para reporte_viewer
+GRANT SELECT ON 5to_Spotify.HistorialReproducción TO 'reporte_viewer'@'localhost';
+GRANT SELECT ON 5to_Spotify.Usuario TO 'reporte_viewer'@'localhost';
+GRANT SELECT ON 5to_Spotify.Suscripcion TO 'reporte_viewer'@'localhost';
+
+-- Aplicar los cambios
+FLUSH PRIVILEGES;
