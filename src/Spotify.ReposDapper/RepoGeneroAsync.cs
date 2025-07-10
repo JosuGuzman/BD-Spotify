3aa2f3b9f605bc336a5dc4c02bc835360ef1f499
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-07-10 15:31:12 -0300

    Create RepoGeneroAsync.cs

---
+++ src/Spotify.ReposDapper/RepoGeneroAsync.cs
@@ -0,0 +1,45 @@
+
+namespace Spotify.ReposDapper;
+
+public class RepoGeneroAsync : RepoGenerico, IRepoGeneroAsync
+{
+    public RepoGeneroAsync(IDbConnection conexion)
+        : base(conexion) {}
+
+    public async Task<Genero> AltaAsync(Genero genero)
+    {
+        var parametros = new DynamicParameters();
+        parametros.Add("@unGenero", genero.genero);
+        parametros.Add("@unidGenero", direction: ParameterDirection.Output);
+
+        await _conexion.ExecuteAsync("altaGenero", parametros, commandType: CommandType.StoredProcedure);
+
+        genero.idGenero = parametros.Get<byte>("@unidGenero");
+        return genero;
+    }
+
+    public async Task<Genero?> DetalleDeAsync(uint idGenero)
+    {
+        var BuscarGeneroPorId = @"SELECT * FROM Genero WHERE idGenero = @idGenero";
+
+        var Buscar = await _conexion.QueryFirstOrDefaultAsync<Genero>(BuscarGeneroPorId, new { idGenero });
+
+        return Buscar;
+    }
+
+    public async Task EliminarAsync(uint idGenero)
+    {
+        string eliminarHistorialReproducciones = @"
+            DELETE FROM HistorialReproducción 
+            WHERE idCancion IN (SELECT idCancion FROM Cancion WHERE idGenero = @idGenero)";
+        await _conexion.ExecuteAsync(eliminarHistorialReproducciones, new { idGenero });
+
+        string eliminarCanciones = @"DELETE FROM Cancion WHERE idGenero = @idGenero";
+        await _conexion.ExecuteAsync(eliminarCanciones, new { idGenero });
+
+        string eliminarGenero = @"DELETE FROM Genero WHERE idGenero = @idGenero";
+        await _conexion.ExecuteAsync(eliminarGenero, new { idGenero });
+    }
+
+    public IList<Genero> Obtener() => EjecutarSPConReturnDeTipoLista<Genero>("ObtenerGeneros").ToList();
+}
