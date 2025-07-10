8f195634a6b1806a5104e25f7a1e2c088ebe7e6c
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-07-10 15:09:45 -0300

    Create RepoAlbumAsync.cs

---
+++ src/Spotify.ReposDapper/RepoAlbumAsync.cs
@@ -0,0 +1,40 @@
+
+namespace Spotify.ReposDapper;
+
+public class RepoAlbumAsync : RepoGenerico,IRepoAlbumAsync
+{
+    public RepoAlbumAsync(IDbConnection conexion)
+        : base(conexion) {}
+
+    public async Task<Album> AltaAsync(Album album)
+    {
+        var parametros = new DynamicParameters();
+        parametros.Add("@unidAlbum", direction: ParameterDirection.Output);
+        parametros.Add("@unTitulo", album.Titulo);
+        parametros.Add("@unidArtista", album.artista.idArtista);
+
+        await _conexion.ExecuteAsync("altaAlbum", parametros, commandType: CommandType.StoredProcedure);
+        album.idAlbum = parametros.Get<uint>("@unidAlbum");
+        return album;
+    }
+
+    public async Task<Album?> DetalleDeAsync(uint idAlbum)
+    {
+        string consultarAlbum = @"SELECT * FROM Album WHERE idAlbum = @idAlbum";
+
+        var Album = await _conexion.QuerySingleOrDefaultAsync<Album>(consultarAlbum, new { idAlbum });
+
+        return Album;
+    }
+
+    public async Task EliminarAsync(uint idAlbum)
+    {
+        string eliminarCanciones = @"DELETE FROM Cancion WHERE idAlbum = @idAlbum";
+        await _conexion.ExecuteAsync(eliminarCanciones, new { idAlbum });
+
+        string eliminarAlbum = @"DELETE FROM Album WHERE idAlbum = @idAlbum";
+        await _conexion.ExecuteAsync(eliminarAlbum, new { idAlbum });
+    }
+
+    public IList<Album> Obtener() => EjecutarSPConReturnDeTipoLista<Album>("ObtenerAlbum").ToList();
+}
