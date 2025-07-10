4fa38a710b60c39af2353c642f734edbb2938b51
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-07-10 15:21:09 -0300

    Update RepoArtista.cs

--- src/Spotify.ReposDapper/RepoArtista.cs
+++ src/Spotify.ReposDapper/RepoArtista.cs
@@ -39,39 +39,4 @@ public class RepoArtista : RepoGenerico, IRepoArtista
 
         return artista;
     }
-
-    public async Task<Artista> AltaAsync(Artista artista)
-    {
-        var parametros = new DynamicParameters();
-        parametros.Add("@unidArtista", direction: ParameterDirection.Output);
-        parametros.Add("@unNombreArtistico", artista.NombreArtistico);
-        parametros.Add("@unNombre", artista.Nombre);
-        parametros.Add("@unApellido", artista.Apellido);
-
-        await _conexion.ExecuteAsync("altaArtista", parametros, commandType: CommandType.StoredProcedure);
-
-        artista.idArtista = parametros.Get<uint>("@unidArtista");
-
-        return artista;
-    }
-
-    public async Task<IEnumerable<Artista>> ObtenerAsync()
-    {
-        return await _conexion.QueryAsync<Artista>("ObtenerArtistas", commandType: CommandType.StoredProcedure);
-    }
-
-    public async Task<Artista?> DetalleDeAsync(uint idArtista)
-    {
-        string consultarArtista = @"SELECT * FROM Artista WHERE idArtista = @idArtista";
-        return await _conexion.QuerySingleOrDefaultAsync<Artista>(consultarArtista, new { idArtista });
-    }
-
-    public async Task EliminarAsync(uint idArtista)
-    {
-        string eliminarAlbum = @"DELETE FROM Album WHERE idArtista = @idArtista";
-        await _conexion.ExecuteAsync(eliminarAlbum, new { idArtista });
-
-        string eliminarArtista = @"DELETE FROM Artista WHERE idArtista = @idArtista";
-        await _conexion.ExecuteAsync(eliminarArtista, new { idArtista });
-    }
 }
