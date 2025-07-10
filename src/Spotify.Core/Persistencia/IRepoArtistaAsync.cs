38c2a518a0a65a96ec43a6500e33f183ab106a3c
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-07-10 15:21:00 -0300

    Update IRepoArtistaAsync.cs

--- src/Spotify.Core/Persistencia/IRepoArtistaAsync.cs
+++ src/Spotify.Core/Persistencia/IRepoArtistaAsync.cs
@@ -1,4 +1,4 @@
 namespace Spotify.Core.Persistencia;
 
-public interface IRepoArtistaAsync : IAltaAsync<Album, uint>, IListado<Artista>, IEliminarAsync<uint>, IDetallePorIdAsync<Album, uint>
+public interface IRepoArtistaAsync : IAltaAsync<Artista, uint>, IListado<Artista>, IEliminarAsync<uint>, IDetallePorIdAsync<Artista, uint>
 {}
