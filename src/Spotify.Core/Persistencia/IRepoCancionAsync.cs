67f05758e475b3435263b1db50d2c2eb7c3fcabd
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-07-10 15:25:27 -0300

    Update IRepoCancionAsync.cs

--- src/Spotify.Core/Persistencia/IRepoCancionAsync.cs
+++ src/Spotify.Core/Persistencia/IRepoCancionAsync.cs
@@ -1,4 +1,4 @@
 namespace Spotify.Core.Persistencia;
 
-public interface IRepoCancionAsync : IAltaAsync<Cancion, uint>, IListado<Cancion>, IEliminarAsync<uint>, IDetallePorIdAsync<Cancion, uint>, IMatcheoAsync
+public interface IRepoCancionAsync : IAltaAsync<Cancion, uint>, IListado<Cancion>, IDetallePorIdAsync<Cancion, uint>, IMatcheoAsync
 {}
