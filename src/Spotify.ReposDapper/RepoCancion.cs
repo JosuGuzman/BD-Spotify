fc06720b0e86283c67856f31629d05304b5992cf
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-07-10 15:24:09 -0300

    Update RepoCancion.cs

--- src/Spotify.ReposDapper/RepoCancion.cs
+++ src/Spotify.ReposDapper/RepoCancion.cs
@@ -2,7 +2,7 @@ namespace Spotify.ReposDapper;
 public class RepoCancion : RepoGenerico, IRepoCancion
 {
     public RepoCancion(IDbConnection conexion)
-        : base(conexion) { }
+        : base(conexion) {}
 
     public uint Alta(Cancion cancion)
     {
@@ -21,23 +21,6 @@ public class RepoCancion : RepoGenerico, IRepoCancion
         return cancion.idCancion;
     }
 
-    public async Task<Cancion> AltaAsync(Cancion cancion)
-    {
-        var parametros = new DynamicParameters();
-        parametros.Add("@unidCancion", direction: ParameterDirection.Output);
-        parametros.Add("@unTitulo", cancion.Titulo);
-        parametros.Add("@unDuration", cancion.Duracion);
-        parametros.Add("@unidAlbum", cancion.album.idAlbum);
-        parametros.Add("@unidArtista", cancion.artista.idArtista);
-        parametros.Add("@unidGenero", cancion.genero.idGenero);
-
-        await _conexion.ExecuteAsync("altaCancion", parametros, commandType: CommandType.StoredProcedure);
-
-        cancion.idCancion = parametros.Get<uint>("@unidCancion");
-
-        return cancion;
-    }
-
     public Cancion? DetalleDe(uint idCancion)
     {
         var BuscarCancionPorId = @"SELECT * FROM Cancion WHERE idCancion = @idCancion";
@@ -45,20 +28,6 @@ public class RepoCancion : RepoGenerico, IRepoCancion
         var Buscar = _conexion.QueryFirstOrDefault<Cancion>(BuscarCancionPorId, new {idCancion});
 
         return Buscar;
-    }
-
-    public async Task<Cancion?> DetalleDeAsync(uint idCancion)
-    {
-        var BuscarCancionPorId = @"SELECT * FROM Cancion WHERE idCancion = @idCancion";
-
-        var Buscar = await _conexion.QueryFirstOrDefaultAsync<Cancion>(BuscarCancionPorId, new { idCancion });
-
-        return Buscar;
-    }
-
-    public Task EliminarAsync(uint id)
-    {
-        throw new NotImplementedException();
     }
 
     public List<string>? Matcheo(string Cadena)
@@ -71,11 +40,5 @@ public class RepoCancion : RepoGenerico, IRepoCancion
         return Lista.ToList();
     }
 
-
     public IList<Cancion> Obtener() => EjecutarSPConReturnDeTipoLista<Cancion>("ObtenerCanciones").ToList();
-
-    public Task<IEnumerable<Cancion>> ObtenerAsync()
-    {
-        throw new NotImplementedException();
-    }
 }
