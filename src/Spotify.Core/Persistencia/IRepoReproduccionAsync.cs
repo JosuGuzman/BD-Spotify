a9968425019a35346888871ee543261c850d6c7c
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-07-15 15:19:53 -0300

    Update IRepoReproduccionAsync.cs

--- src/Spotify.Core/Persistencia/IRepoReproduccionAsync.cs
+++ src/Spotify.Core/Persistencia/IRepoReproduccionAsync.cs
@@ -1,4 +1,4 @@
 namespace Spotify.Core.Persistencia;
 
-public interface IRepoReproduccionAsync : IAltaAsync<Reproduccion, uint>, IListado <Reprosuccion>,   IDetallePorIdAsync<Reproduccion, uint>
+public interface IRepoReproduccionAsync : IAltaAsync<Reproduccion, uint>, IListado <Reproduccion>,   IDetallePorIdAsync<Reproduccion, uint>
 {}
