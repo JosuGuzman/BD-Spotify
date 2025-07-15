bfd4b23c670d6034a9b93abd62adf5996eeb8800
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-07-15 16:49:42 -0300

    update RepoReproduccion.cs

--- src/Spotify.ReposDapper/RepoReproduccion.cs
+++ src/Spotify.ReposDapper/RepoReproduccion.cs
@@ -20,21 +20,6 @@ public class RepoReproduccion : RepoGenerico, IRepoReproduccion
         return reproduccion.IdHistorial;
     }
 
-    public async Task<Reproduccion> AltaAsync(Reproduccion reproduccion)
-    {
-        var parametros = new DynamicParameters();
-        parametros.Add("@unidHistorial", direction: ParameterDirection.Output);
-        parametros.Add("@unidUsuario", reproduccion.usuario.idUsuario);
-        parametros.Add("@unidCancion", reproduccion.cancion.idCancion);
-        parametros.Add("@unFechaReproduccion", reproduccion.FechaReproduccion);
-
-        await _conexion.ExecuteAsync("altaHistorial_reproduccion", parametros, commandType: CommandType.StoredProcedure);
-
-        reproduccion.IdHistorial = parametros.Get<uint>("@unidHistorial");
-
-        return reproduccion;
-    }
-
     public Reproduccion? DetalleDe(uint idHistorial)
     {
         var BuscarReproduccionPorId = @"SELECT * FROM HistorialReproduccion WHERE idHistorial = @idHistorial";
@@ -44,24 +29,5 @@ public class RepoReproduccion : RepoGenerico, IRepoReproduccion
         return Buscar;
     }
 
-    public async Task<Reproduccion?> DetalleDeAsync(uint idHistorial)
-    {
-        var BuscarReproduccionPorId = @"SELECT * FROM HistorialReproduccion WHERE idHistorial = @idHistorial";
-
-        var Buscar = await _conexion.QueryFirstOrDefaultAsync<Reproduccion>(BuscarReproduccionPorId, new { idHistorial });
-
-        return Buscar;
-    }
-
-    public Task EliminarAsync(uint id)
-    {
-        throw new NotImplementedException();
-    }
-
     public IList<Reproduccion> Obtener() => EjecutarSPConReturnDeTipoLista<Reproduccion>("ObtenerHistorialReproduccion").ToList();
-
-    public Task<IEnumerable<Reproduccion>> ObtenerAsync()
-    {
-        throw new NotImplementedException();
-    }
 }
