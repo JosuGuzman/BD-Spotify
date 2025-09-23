e389dbe67adf27caa3cbd1f8604683d6fcd67bf6
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-09-23 15:28:30 -0300

    Delete Album.cs

--- src/Spotify.Mvc/Models/Album.cs
+++
@@ -1,9 +0,0 @@
-namespace Spotify.Mvc.Models;
-
-public class Album
-{
-    public uint idAlbum { get; set; }
-    public required string Titulo { get; set; }
-    public DateTime FechaLanzamiento { get; set; }
-    public required Artista artista { get; set; }
-}
