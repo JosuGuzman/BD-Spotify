0f3676e6d9285c9cdc38bc0ac889f159bd304106
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-10-30 15:28:16 -0300

    Update Genero.cs

--- src/Spotify.Core/Genero.cs
+++ src/Spotify.Core/Genero.cs
@@ -1,7 +1,9 @@
-namespace Spotify.Core;
-
-public class Genero
+namespace Spotify.Core
 {
-    public byte idGenero {get; set;}
-    public required string genero {get; set;}
+    public class Genero
+    {
+        public byte idGenero { get; set; }
+        public required string genero { get; set; }
+        public string Descripcion { get; set; } = string.Empty; // Nueva propiedad
+    }
 }
