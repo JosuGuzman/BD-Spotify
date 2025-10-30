71c4e409407fcabe71832ed0b2127bb89d8e3106
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-10-30 15:28:12 -0300

    Update Cancion.cs

--- src/Spotify.Core/Cancion.cs
+++ src/Spotify.Core/Cancion.cs
@@ -2,11 +2,12 @@ namespace Spotify.Core
 {
     public class Cancion
     {
-        public uint idCancion {get;set;}
-        public required string Titulo {get;set;}
-        public TimeSpan Duracion {get;set;}
-        public required Album album {get;set;}
-        public required Genero genero {get;set;}
-        public required Artista artista {get;set;}
+        public uint idCancion { get; set; }
+        public required string Titulo { get; set; }
+        public TimeSpan Duracion { get; set; }
+        public required Album album { get; set; }
+        public required Genero genero { get; set; }
+        public required Artista artista { get; set; }
+        public string ArchivoMP3 { get; set; } = string.Empty;
     }
 }
