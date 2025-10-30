b9a07feb64921a8eeaf4db80e66ce7caefa86016
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-10-30 15:28:08 -0300

    Update Artista.cs

--- src/Spotify.Core/Artista.cs
+++ src/Spotify.Core/Artista.cs
@@ -2,9 +2,9 @@ namespace Spotify.Core
 {
     public class Artista
     {
-        public uint idArtista {get;set;}
-        public required string NombreArtistico {get;set;}
-        public required string Nombre {get;set;}
-        public required string Apellido {get;set;}
+        public uint idArtista { get; set; }
+        public required string NombreArtistico { get; set; }
+        public required string Nombre { get; set; }
+        public required string Apellido { get; set; }
     }
 }
