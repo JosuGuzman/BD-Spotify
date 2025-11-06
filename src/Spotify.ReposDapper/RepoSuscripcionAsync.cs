b024e044b052723697d3771c38aa0c865348021f
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-11-06 14:02:03 -0300

    Update RepoSuscripcionAsync.cs

--- src/Spotify.ReposDapper/RepoSuscripcionAsync.cs
+++ src/Spotify.ReposDapper/RepoSuscripcionAsync.cs
@@ -3,9 +3,9 @@ namespace Spotify.ReposDapper;
 public class RepoSuscripcionAsync : RepoGenerico, IRepoRegistroAsync
 {
     public RepoSuscripcionAsync(IDbConnection conexion) 
-        : base(conexion) { }
+        : base(conexion) {}
 
-    public Task<Registro> AltaAsync(Registro registro)
+    public async Task<Registro> AltaAsync(Registro registro)
     {
         var parametros = new DynamicParameters();
         parametros.Add("@unidSuscripcion", direction: ParameterDirection.Output);
@@ -15,16 +15,16 @@ public class RepoSuscripcionAsync : RepoGenerico, IRepoRegistroAsync
         
         await _conexion.ExecuteAsync("altaRegistroSuscripcion", parametros, commandType: CommandType.StoredProcedure);
         registro.idSuscripcion = parametros.Get<uint>("@unidSuscripcion");
-        return registro.idSuscripcion;
+        return registro;
     }
 
-    public Task<Registro?> DetalleDeAsync(uint id)
+    public async Task<Registro?> DetalleDeAsync(uint id)
     {
         var sql = @"SELECT * FROM Suscripcion WHERE idSuscripcion = @idSuscripcion";
-        return await _conexion.QueryFirstOrDefaultAsync<Registro>(sql, new { idSuscripcion });
+        return await _conexion.QueryFirstOrDefaultAsync<Registro>(sql, new { idSuscripcion = id });
     }
 
-    public Task<List<Registro>> Obtener()
+    public async Task<List<Registro>> Obtener()
     {
         var resultados = await EjecutarSPConReturnDeTipoListaAsync<Registro>("ObtenerSuscripciones");
         return resultados.ToList();
